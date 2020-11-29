9e69b685c87eb70fd8dcc1a36357ebc2ae0eff7a
SergioFdezRc <s.fernandez.rincon@gmail.com>
2020-11-29 17:04:52 +0100

    Se ha integrado la API con el repositorio de la base de datos.

--- swagger_server/controllers/door_controller.py
+++ swagger_server/controllers/door_controller.py
@@ -1,18 +1,9 @@
 import connexion
-import six
+from flask import jsonify
+from muii_g2_family_lock_database.Database import PostgresDB
 
 from swagger_server.models.door import Door  # noqa: E501
 from swagger_server.models.update_door import UpdateDoor  # noqa: E501
-from swagger_server import util
-
-import os
-import psycopg2
-
-DATABASE_URL = os.environ['DATABASE_URL']
-DATABASE_HOST = os.environ['DATABASE_HOST']
-DATABASE_USER = os.environ['DATABASE_USER']
-DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
-DATABASE_NAME = os.environ['DATABASE_NAME']
 
 
 def add_door(door):  # noqa: E501
@@ -28,31 +19,8 @@ def add_door(door):  # noqa: E501
     if connexion.request.is_json:
         door = Door.from_dict(connexion.request.get_json())  # noqa: E501
 
-    connection = psycopg2.connect(user=DATABASE_USER,
-                                  password=DATABASE_PASSWORD,
-                                  host=DATABASE_HOST,
-                                  port="5432",
-                                  database=DATABASE_NAME)
-    cursor = connection.cursor()
-
-    try:
-        postgres_insert_query = '''INSERT INTO door (name) VALUES (%s)'''
-        cursor.execute(postgres_insert_query, (door.name,))
-
-        connection.commit()
-        count = cursor.rowcount
-        print(count, "Record inserted successfully into mobile table")
-
-    except (Exception, psycopg2.Error) as error:
-        if connection:
-            print("Failed to insert record into mobile table", error)
-
-    finally:
-        # closing database connection.
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
+    db = PostgresDB()
+    db.add_new_door(door.name)
 
     return "Door inserted successfully"
 
@@ -67,32 +35,8 @@ def delete_door(door_id):  # noqa: E501
 
     :rtype: str
     """
-
-    connection = psycopg2.connect(user=DATABASE_USER,
-                                  password=DATABASE_PASSWORD,
-                                  host=DATABASE_HOST,
-                                  port="5432",
-                                  database=DATABASE_NAME)
-    cursor = connection.cursor()
-
-    try:
-        # Update single record now
-        sql_delete_query = """DELETE FROM door WHERE id = %s"""
-        cursor.execute(sql_delete_query, (door_id,))
-        connection.commit()
-        count = cursor.rowcount
-        print(count, "Record deleted successfully ")
-
-    except (Exception, psycopg2.Error) as error:
-        print("Error in Delete operation", error)
-
-    finally:
-        # closing database connection.
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
-
+    db = PostgresDB()
+    db.delete_door(door_id)
     return 'Door deleted successfully'
 
 
@@ -104,30 +48,10 @@ def get_all_doors_state():  # noqa: E501
 
     :rtype: str
     """
+    db = PostgresDB()
+    doors = db.get_all_doors_state()
 
-    connection = psycopg2.connect(user=DATABASE_USER,
-                                  password=DATABASE_PASSWORD,
-                                  host=DATABASE_HOST,
-                                  port="5432",
-                                  database=DATABASE_NAME)
-    cursor = connection.cursor()
-
-    try:
-        sql_select_query = """SELECT * FROM door"""
-        cursor.execute(sql_select_query)
-        record = cursor.fetchall()
-
-    except (Exception, psycopg2.Error) as error:
-        print("Error in update operation", error)
-
-    finally:
-        # closing database connection.
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
-
-    return record
+    return jsonify({"doors": doors})
 
 
 def get_door_state(door_id):  # noqa: E501
@@ -140,30 +64,9 @@ def get_door_state(door_id):  # noqa: E501
 
     :rtype: str
     """
-
-    connection = psycopg2.connect(user=DATABASE_USER,
-                                  password=DATABASE_PASSWORD,
-                                  host=DATABASE_HOST,
-                                  port="5432",
-                                  database=DATABASE_NAME)
-    cursor = connection.cursor()
-
-    try:
-        sql_select_query = """SELECT * FROM door WHERE id = %s"""
-        cursor.execute(sql_select_query, (door_id,))
-        record = cursor.fetchall()
-
-    except (Exception, psycopg2.Error) as error:
-        print("Error in update operation", error)
-
-    finally:
-        # closing database connection.
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
-
-    return record
+    db = PostgresDB()
+    door = db.get_door_state(door_id)
+    return jsonify({"door": door})
 
 
 def update_door_state(door):  # noqa: E501
@@ -176,38 +79,9 @@ def update_door_state(door):  # noqa: E501
 
     :rtype: str
     """
-    print('##################################')
-    print(connexion.request.get_json())
-    print('##################################')
     if connexion.request.is_json:
         door = UpdateDoor.from_dict(connexion.request.get_json())  # noqa: E501
-    print('##################################')
-    print(door)
-    print('##################################')
 
-    connection = psycopg2.connect(user=DATABASE_USER,
-                                  password=DATABASE_PASSWORD,
-                                  host=DATABASE_HOST,
-                                  port="5432",
-                                  database=DATABASE_NAME)
-    cursor = connection.cursor()
-
-    try:
-        # Update single record now
-        sql_update_query = """UPDATE door SET state = %s WHERE name = %s"""
-        cursor.execute(sql_update_query, (door.state, door.name))
-        connection.commit()
-        count = cursor.rowcount
-        print(count, "Record Updated successfully ")
-
-    except (Exception, psycopg2.Error) as error:
-        print("Error in update operation", error)
-
-    finally:
-        # closing database connection.
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
-
+    db = PostgresDB()
+    db.update_door_state(door.state, door.name)
     return 'Door updated successfully'
--- swagger_server/test/test_door_controller.py
+++ swagger_server/test/test_door_controller.py
@@ -2,8 +2,9 @@
 
 from __future__ import absolute_import
 
+from unittest import mock
+
 from flask import json
-from six import BytesIO
 
 from swagger_server.models.door import Door  # noqa: E501
 from swagger_server.models.update_door import UpdateDoor  # noqa: E501
@@ -13,64 +14,83 @@ from swagger_server.test import BaseTestCase
 class TestDoorController(BaseTestCase):
     """DoorController integration test stubs"""
 
-    def test_add_door(self):
+    @mock.patch("muii_g2_family_lock_database.Database.PostgresDB.add_new_door")
+    def test_add_door(self, mocked_add_new_door):
         """Test case for add_door
 
         Add a new door to the system
         """
         body = Door()
+        mocked_add_new_door.assert_not_called()
         response = self.client.open(
             '/door',
             method='POST',
             data=json.dumps(body),
             content_type='application/json')
+        mocked_add_new_door.assert_called_once()
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_delete_door(self):
+    @mock.patch("muii_g2_family_lock_database.Database.PostgresDB.delete_door")
+    def test_delete_door(self, mocked_delete_door):
         """Test case for delete_door
 
         Delete a door
         """
+        mocked_delete_door.assert_not_called()
         response = self.client.open(
             '/door/{id}'.format(id=56),
             method='DELETE')
+        mocked_delete_door.assert_called_once()
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_get_all_doors_state(self):
+    @mock.patch("muii_g2_family_lock_database.Database.PostgresDB.get_all_doors_state")
+    def test_get_all_doors_state(self, mocked_get_all_doors_state):
         """Test case for get_all_doors_state
 
         Get all doors state
         """
+        mocked_get_all_doors_state.assert_not_called()
+        mocked_get_all_doors_state.return_value = [
+
+        ]
         response = self.client.open(
             '/door',
             method='GET')
+        mocked_get_all_doors_state.assert_called_once()
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_get_door_state(self):
+    @mock.patch("muii_g2_family_lock_database.Database.PostgresDB.get_door_state")
+    def test_get_door_state(self, mocked_get_door_state):
         """Test case for get_door_state
 
         Get a door state
         """
+        mocked_get_door_state.assert_not_called()
+        mocked_get_door_state.return_value = []
         response = self.client.open(
             '/door/{id}'.format(id=56),
             method='GET')
+        mocked_get_door_state.assert_called_once()
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_update_door_state(self):
+    @mock.patch("muii_g2_family_lock_database.Database.PostgresDB.update_door_state")
+    def test_update_door_state(self, mocked_update_door_state):
         """Test case for update_door_state
 
         Update door state
         """
+        mocked_update_door_state.assert_not_called()
         body = UpdateDoor()
         response = self.client.open(
             '/door',
             method='PUT',
             data=json.dumps(body),
             content_type='application/json')
+        mocked_update_door_state.assert_called_once()
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
